79ef7f615ea98988d0f5a29832efdc3ef74d08e0
mohammadzm0101 <zm441572@gmail.com>
2022-12-10 10:54:29 +0330

    all

--- js/single.js
+++ js/single.js
@@ -7,34 +7,29 @@
 //     return object.values(pi);
 // }
 // console.log(perInformation(prompt('please entet your first name'),prompt('please entery your birth province'),prompt('please enter your postal code')))
-const address = addressA('hii', 'heee', 'heere');
-const address1 = addressA('hii', 'hoooy', 'heee');
-function addressA(aa, bb, cccc) {
+const address = addressA('hii', 'hoooy', 'heere');
+const address1 = addressA('hiei', 'hoooy', 'heere');
+function addressA(aa, bb, cc) {
     return {
-        a : aa,
-        b : bb,
-        c : cccc
-    }   
-}
-console.log(address);
-// console.log(address1);
-// let comparson = JSON.stringify(address) === JSON.stringify(address1);
-// console.log(comparson);
-let samePro = {};
-for (const key in address) {
-    if (Object.hasOwnProperty.call(address, key)) {
-    const element = address[key];
-        console.log(element);
-        // for (const key1 in address1) {
-        //     if (Object.hasOwnProperty.call(address1, key1)) {
-        //         const element1 = address1[key1];
-        //         console.log(element1)
-                
-        //     }
-        // }
-        for (let index = 0; index < address1.length; index++) {
-            const element1 = address1[index];  
-            if 
-        } 
+        a: aa,
+        b: bb,
+        c: cc
     }
 }
+console.log(address);
+console.log(address1);
+function compare(address, address1) {
+    let comparson = JSON.stringify(address) === JSON.stringify(address1);
+    let noting = 'dont be compeletly same got step 2';
+    if (comparson === true) {
+        return comparson;
+    }else {
+      return noting
+    }
+};
+console.log(compare(address, address1));
+function deepCompare(params) {
+    for (let i = 0; i < address.length; i++) {
+         array[i]; 
+    }
+};
