ae694f86c3151ba8fd4b5ff59b269362fe3f1855
mohammadzm0101 <zm441572@gmail.com>
2022-12-26 12:44:47 +0330

    forth train solve

--- js/single.js
+++ js/single.js
@@ -6,29 +6,49 @@
 //     }
 //     return object.values(pi);
 // }
+// next train
 // console.log(perInformation(prompt('please entet your first name'),prompt('please entery your birth province'),prompt('please enter your postal code')))
-const address = addressA('hii', 'hoooy', 'heere');
-const address1 = addressA('hii', 'hoooy', 'heere');
-function addressA(aa, bb, cc) {
-    return {
-        a: aa,
-        b: bb,
-        c: cc
-    }
-}
-console.log(address);
-console.log(address1);
-function compare(address, address1) {
-    let comparson = JSON.stringify(address) === JSON.stringify(address1);
-    if (comparson === true) {
-        return 'yeh';
-    }else {
-      return 'no'
-    }
-};
-console.log(compare(address, address1));
-function deepCompare(address, address1) {
-   const keys1 = Object.keys(address);
-   return keys1
-};
-console.log(deepCompare(address, address1));
+// let address = addressA('hii', 'hoooy', 'hoooy');
+// let address1 = addressA('hii', 'hoooy', 'heere');
+// function addressA(aa, bb, cc) {
+//     return {
+//         a: aa,
+//         b: bb,
+//         c: cc
+//     }
+// }
+
+
+// function compare(address, address1) {
+//     let comparson = JSON.stringify(address) === JSON.stringify(address1);
+//     if (comparson === true) {
+//         return 'yeh';
+//     } else {
+//         return 'no'
+//     }
+// };
+// console.log(compare(address, address1));
+// function deepCompare(address, address1) {
+//     let h = [];
+//     let h1 = [];
+//     for (const key1 in address1) {
+//         const element1 = address1[key1];
+
+//         for (let key in address) {
+
+//             if (address[key] == element1) {
+//                 h1.push(address[key])
+//                 delete address[key]
+
+//             };
+//         };
+//     };
+//     console.log(h1);
+//     let uniquechars = [... new Set(h1)];
+//     console.log(uniquechars);
+//     console.log(address);
+//     console.log(address1);
+// }
+// console.log(deepCompare(address, address1));
+// next example
+
